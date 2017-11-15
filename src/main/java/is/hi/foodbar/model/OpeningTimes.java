f4b3b517390d78e64cd5e18a30d6d1788d54d2b6
elk11 <elvar@europe.com>
2017-11-15 16:20:01 +0000

    ný aðferð í OpeningTimes, getTimes() gefur tímana á hverjum dag sem fylki þar sem 0 er mánudagur, 1 þriðjudagur o.s.fr.

--- src/main/java/is/hi/foodbar/model/OpeningTimes.java
+++ src/main/java/is/hi/foodbar/model/OpeningTimes.java
@@ -276,14 +276,25 @@ public class OpeningTimes {
         return s;
     }
 
+    public String[] getTimes() {
+        String[] s = {formatNum(mondayOpen)+"-"+formatNum(mondayClosed),
+                formatNum(tuesdayOpen)+"-"+formatNum(tuesdayClosed),
+                formatNum(wednesdayOpen)+"-"+formatNum(wednesdayClosed),
+                formatNum(thursdayOpen)+"-"+formatNum(thursdayClosed),
+                formatNum(fridayOpen)+"-"+formatNum(fridayClosed),
+                formatNum(saturdayOpen)+"-"+formatNum(saturdayClosed),
+                formatNum(sundayOpen)+"-"+formatNum(sundayClosed)};
+        return s;
+    }
+
     @Override
     public String toString() {
-        return "Mon: "+formatNum(mondayOpen)+"-"+formatNum(mondayClosed)+
-                " Tue: "+formatNum(tuesdayOpen)+"-"+formatNum(tuesdayClosed)+
-                " Wed: "+formatNum(wednesdayOpen)+"-"+formatNum(wednesdayClosed)+
-                " Thu: "+formatNum(tuesdayOpen)+"-"+formatNum(thursdayClosed)+
-                " Fri: "+formatNum(fridayOpen)+"-"+formatNum(fridayClosed)+
-                " Sat: "+formatNum(saturdayOpen)+"-"+formatNum(saturdayClosed)+
-                " Sun: "+formatNum(sundayOpen)+"-"+formatNum(sundayClosed);
+        return "Mon: "+formatNum(mondayOpen)+"-"+formatNum(mondayClosed)+", \n"+
+                " Tue: "+formatNum(tuesdayOpen)+"-"+formatNum(tuesdayClosed)+", \n"+
+                " Wed: "+formatNum(wednesdayOpen)+"-"+formatNum(wednesdayClosed)+", \n"+
+                " Thu: "+formatNum(thursdayOpen)+"-"+formatNum(thursdayClosed)+", \n"+
+                " Fri: "+formatNum(fridayOpen)+"-"+formatNum(fridayClosed)+", \n"+
+                " Sat: "+formatNum(saturdayOpen)+"-"+formatNum(saturdayClosed)+", \n"+
+                " Sun: "+formatNum(sundayOpen)+"-"+formatNum(sundayClosed)+", \n";
     }
 }
